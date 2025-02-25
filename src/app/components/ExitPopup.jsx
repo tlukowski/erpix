0185e1eb15616339354cf8c4a3c4d04f1ab8774e
tlukowski <tomeklukowski@gmail.com>
2025-02-25 20:21:25 +0100

    Exit

--- src/app/components/ExitPopup.jsx
+++ src/app/components/ExitPopup.jsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { useModal } from "../context/ModalContext";
+import { motion, AnimatePresence } from "framer-motion";
+
 const ExitPopup = () => {
   const { openModal } = useModal();
   const [isVisible, setIsVisible] = useState(false);
@@ -47,29 +49,35 @@ const ExitPopup = () => {
     setIsVisible(false);
   };
 
-  if (!isVisible) return null;
-
   return (
-    <div className="fixed bottom-0 w-full  flex justify-center items-center z-10">
-      <div className="bg-gray-100 shadow-2xl w-full max-w-full p-6 rounded-lg text-center">
-        <div>
-          <h2 className="text-xl font-bold">Potrzebuje pomocy z wyborem?</h2>
-          <p className="mt-2">Zadzwoń lub zostaw kontakt!</p>
-          <button
-            onClick={openModal}
-            className="mt-4 bg-secondary hover:bg-black transition-colors text-white px-4 py-2 rounded mr-2"
-          >
-            Zostaw kontakt
-          </button>
-          <button
-          className="mt-4 bg-blue-500 transition-colors text-white px-4 py-2 rounded hover:bg-blue-600"
-          onClick={closePopup}
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div 
+          className="fixed bottom-0 w-full flex justify-center items-center z-10"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ duration: 0.5 }}
         >
-          Zamknij
-        </button>
-        </div>
-      </div>
-    </div>
+          <div className="bg-gray-100 shadow-2xl w-full max-w-full p-6 rounded-lg text-center">
+            <h2 className="text-xl font-bold">Potrzebuje pomocy z wyborem?</h2>
+            <p className="mt-2">Zadzwoń lub zostaw kontakt!</p>
+            <button
+              onClick={openModal}
+              className="mt-4 bg-secondary hover:bg-black transition-colors text-white px-4 py-2 rounded mr-2"
+            >
+              Zostaw kontakt
+            </button>
+            <button
+              className="mt-4 bg-blue-500 transition-colors text-white px-4 py-2 rounded hover:bg-blue-600"
+              onClick={closePopup}
+            >
+              Zamknij
+            </button>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
